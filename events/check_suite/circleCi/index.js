b45daef33bbc0d6659f94d48791251e7e5b8749c
Daniel Rodríguez <sadasant@gmail.com>
2018-07-01 05:33:34 +0000

    event handler done, now we need to fetch and format circleci

--- events/check_suite/circleCi/index.js
+++ events/check_suite/circleCi/index.js
@@ -1,10 +1,17 @@
 let circleci = require('../../../ci/circleci')
 
 // check_suite circleCi
+//
+// This event handler is expected to be called when a CI build finishes,
+// it updates the pull request body with a formatted summary of the status
+// of the CircleCI build.
+//
 module.exports = async context => {
   const { owner, repo } = context.repo()
-  const [number] = context.payload.check_suite.pull_requests
-  let { body } = await context.github.pullRequests.get({ owner, repo, number })
+  const [{ number }] = context.payload.check_suite.pull_requests
+  let {
+    data: { body }
+  } = await context.github.pullRequests.get({ owner, repo, number })
   let status = await circleci.getStatus()
   body = `${body}\n${status}`
   await context.github.pullRequests.update({ owner, repo, number, body })
--- events/check_suite/circleCi/index.test.js
+++ events/check_suite/circleCi/index.test.js
@@ -6,6 +6,7 @@ describe('Checking CircleCI', () => {
   let app
   let github
   let body = 'Pull Request Body'
+  let pullRequests = payload.check_suite.pull_requests
 
   beforeEach(() => {
     app = new Application()
@@ -13,16 +14,17 @@ describe('Checking CircleCI', () => {
     // This is an easy way to mock out the GitHub API
     github = {
       pullRequests: {
-        async get() {
+        async get({ number }) {
+          let foundPR = pullRequests.find(x => x.number === number)
+          if (!foundPR) throw 'Invalid PR Number'
           return {
-            body
+            data: {
+              ...foundPR,
+              body
+            }
           }
         },
-        update: jest.fn().mockReturnValue(
-          Promise.resolve({
-            // Whatever the GitHub API should return
-          })
-        )
+        update: jest.fn().mockReturnValue(Promise.resolve({}))
       }
     }
     // Passes the mocked out GitHub API into out app instance
@@ -33,6 +35,9 @@ describe('Checking CircleCI', () => {
     it('performs an action', async () => {
       await app.receive({ event: 'check_suite', payload })
       expect(github.pullRequests.update).toHaveBeenCalled()
+      expect(
+        github.pullRequests.update.mock.calls[0][0].body.indexOf(body)
+      ).toBe(0)
       expect(github.pullRequests.update.mock.calls[0][0].body).not.toBe(body)
     })
   })

4a85bb830f0192bb1616f54f7d95f07bd19f4904
Daniel Rodríguez <sadasant@gmail.com>
2018-07-02 21:30:40 +0000

    Trying to fix the double message stuff

--- events/check_suite/circleCi/index.test.js
+++ events/check_suite/circleCi/index.test.js
@@ -5,10 +5,11 @@ const payload = require('../../../fixtures/pr_check_suite')
 describe('Checking CircleCI', () => {
   let app
   let github
-  let body = 'Pull Request Body'
+  let body
   let pullRequests = payload.check_suite.pull_requests
 
   beforeEach(() => {
+    body = 'Pull Request Body'
     app = new Application()
     app.load(plugin)
     // This is an easy way to mock out the GitHub API
@@ -31,14 +32,35 @@ describe('Checking CircleCI', () => {
     app.auth = () => Promise.resolve(github)
   })
 
-  describe('your functionality', () => {
-    it('performs an action', async () => {
+  describe("should update the PR's body", () => {
+    it('should work one time', async () => {
       await app.receive({ event: 'check_suite', payload })
       expect(github.pullRequests.update).toHaveBeenCalled()
       expect(
         github.pullRequests.update.mock.calls[0][0].body.indexOf(body)
       ).toBe(0)
-      expect(github.pullRequests.update.mock.calls[0][0].body).not.toBe(body)
+      expect(github.pullRequests.update.mock.calls[0][0].body).toBe(`Pull Request Body<span data="probot-comment-start"></span>
+PR ROBOT :robot:
+---
+
+CircleCI Passed! :clap::white_check_mark:<span data="probot-comment-end"></span>`)
+    })
+    it('should work two times', async () => {
+      await app.receive({ event: 'check_suite', payload })
+      body = github.pullRequests.update.mock.calls[0][0].body
+      await app.receive({ event: 'check_suite', payload })
+      expect(github.pullRequests.update).toHaveBeenCalled()
+      expect(
+        github.pullRequests.update.mock.calls[1][0].body.indexOf(body)
+      ).toBe(0)
+      expect(github.pullRequests.update.mock.calls[1][0].body).toBe(`Pull Request Body<span data="probot-comment-start"></span>
+PR ROBOT :robot:
+
+ MAKING THE TESTS FAIL JUST TO TRY SOMETHING
+
+---
+
+CircleCI Passed! :clap::white_check_mark:<span data="probot-comment-end"></span>`)
     })
   })
 })
